9f46fbe72e6fa92f11623534b7770127bec46fcc
ipraveendn <praveendn1230@gmail.com>
2025-05-14 19:39:14 +0530

    herosec_done

---
+++ Flacipy/src/Components/Navbar/Herosec/Herosec.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Herosec.css";
+const Herosec = () => {
+  return (
+    <div className="parent">
+    <div className="hero-sec">
+      <h2>Prepare, Practice, and Land Your Dream Job.</h2>
+      <p>
+        Complete placement preparation with mock interviews, company-specific
+        questions, skill practice and resume building tools.
+      </p>
+      <buttons className="buttons-hero-sec">
+        <button className="get-started">Get Started</button>
+        <button className="learn-more">Learn More</button>
+      </buttons>
+    </div>
+    </div>
+  );
+};
+
+export default Herosec;
